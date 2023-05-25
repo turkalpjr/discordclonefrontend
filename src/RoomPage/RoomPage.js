5d40d91118aa51686cbdf941c75ed0cabd9cb6ba
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-05-25 17:12:47 +0300

    room page devam
    room page devam

--- src/RoomPage/RoomPage.js
+++ src/RoomPage/RoomPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import ParticipantsSection from './ParticipantsSection/ParticipantsSection';
+import VideoSection from './VideoSection/VideoSection';
+import ChatSection from './ChatSection/ChatSection';
+import RoomLabel from './RoomLabel';
 
+import './RoomPage.css';
 const RoomPage = () => {
     return (
-        <div>
-            hello this is room page
+        <div className='room_container'>
+            <ParticipantsSection />
+            <VideoSection />
+            <ChatSection />
+            <RoomLabel roomId='1234' />
         </div>
     );
 };
-
 export default RoomPage;
---
+++ src/RoomPage/VideoSection/VideoSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoSection = () => {
+    return (
+        <div className='video_section_container'>
+            video section
+        </div>
+    );
+};
+
+export default VideoSection;
