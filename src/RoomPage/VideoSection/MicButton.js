84a31f3b208e11d7fa6bcfec4a9419550f2e6606
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-05-26 10:30:57 +0300

    mic camera screensharing buttons ok
    mic camera screensharing buttons ok

--- src/RoomPage/VideoSection/MicButton.js
+++ src/RoomPage/VideoSection/MicButton.js
@@ -1,9 +1,15 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import MicButtonImg from '../../resources/images/mic.svg';
+import MicButtonImgOff from '../../resources/images/micOff.svg';
 const MicButton = () => {
+
+    const [isMicMuted, setIsMicMuted] = useState(false);
+    const handleMicButtonPressed = () => {
+      setIsMicMuted(!isMicMuted);
+    }
     return (
-        <div>
-            Mic Button
+        <div className='video_button_container'>
+            <img src={isMicMuted ? MicButtonImgOff : MicButtonImg} onClick={handleMicButtonPressed} className='video_button_image' />
         </div>
     );
 };
