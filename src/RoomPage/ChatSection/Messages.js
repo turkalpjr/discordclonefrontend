cbad8648ace0f34e632ce784e60d29e3b1189578
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-05-29 15:59:47 +0300

    chat_messages_start
    chat_messages_start

---
+++ src/RoomPage/ChatSection/Messages.js
@@ -0,0 +1,53 @@
+import React from 'react';
+const messages = [{
+    content: 'Hey',
+    identity: 'Marek',
+    messageCreatedByMe: true
+},
+{
+    content: 'hello',
+    identity: 'Marek',
+    messageCreatedByMe: true
+},
+{
+    content: 'hello marek',
+    identity: 'John',
+    messageCreatedByMe: false
+},
+{
+    content: 'hello everyone',
+    identity: 'Anna',
+    messageCreatedByMe: false
+}
+];
+
+const Message = ({ author, content, sameAuthor, messageCreatedByMe }) => {
+    const alignClass = messageCreatedByMe ? 'message_align_right' : 'message_align_left';
+    const authorText = messageCreatedByMe ? 'You' : author;
+    const contentAdditionalStyles = messageCreatedByMe ? 'message_right_styles' : 'message_left_styles';
+    return (
+        <div className={`message_container ${alignClass}`}>
+            {!sameAuthor && <p className='message_title'>{authorText}</p>}
+            <p className={`message_content ${contentAdditionalStyles}`}>{content}</p>
+        </div>
+    );
+}
+const Messages = () => {
+    return (<div className='messages_container'>
+        {messages.map((message, index) => {
+
+            const sameAuthor = index > 0 && message.identity === messages[index - 1].identity;
+            return (
+                <Message
+                    key={`${message.content}${index}`}
+                    author={message.identity}
+                    content={message.content}
+                    sameAuthor={sameAuthor}
+                    messageCreatedByMe={message.messageCreatedByMe}
+                />
+            );
+        })}
+    </div>
+    )
+};
+export default Messages;
